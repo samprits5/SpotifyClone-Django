a6fbf14114841ea1b9e0d42b3876e33aedfa098e
Samprit Sarkar <Samprit@Samprits-Mac.local>
2019-11-07 16:54:30 +0530

    Player Section Completed

--- frontend/webplayer/views.py
+++ frontend/webplayer/views.py
@@ -1,26 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from admin.homepage.models import Homepage
 from admin.song.models import Song
+import random
 # Create your views here.
 
+def find_song(song_id):
+
+	song = Song.objects.filter(pk=song_id)
+
+	if not song:
+		return False
+	else:
+		song = song.get()
+
+	song_ids = Song.objects.values_list('id', flat=True)
+
+	sid_index = list(song_ids).index(song_id)
+
+	if sid_index == 0:
+
+		prev_id = '-1'
+		next_id = str(song_ids[sid_index+1])
+
+	elif sid_index == len(song_ids) - 1:
+
+		prev_id = str(song_ids[sid_index-1])
+		next_id = '-1'
+
+	else:
+		prev_id = str(song_ids[sid_index-1])
+		next_id = str(song_ids[sid_index+1])
+
+	return song, prev_id, next_id
+
+def random_song_id():
+	song_ids = Song.objects.values_list('id', flat=True)
+
+	return random.choice(list(song_ids))
+
 def index(request):
-	
-	data = Homepage.objects.all()
-
-	song = Song.objects.filter(pk=2).get()
-
-	return render(request, 'frontendTemplates/webplayer/index.html', {'data':data, 'song':song})
+	return redirect('player-index-id', sid=random_song_id())
 
 
 def index_id(request, sid):
 	
 	data = Homepage.objects.all()
 
-	song = Song.objects.filter(pk=sid)
-
-	if not song:
-		song = Song.objects.filter(pk=2).get()
+	if find_song(sid):
+		song, prev_id, next_id = find_song(sid)
 	else:
-		song = song.get()
+		return redirect('player-index-id', sid=random_song_id())
 
-	return render(request, 'frontendTemplates/webplayer/index.html', {'data':data, 'song':song})
+	return render(request, 'frontendTemplates/webplayer/index.html', {'data':data, 'song':song, 'pid': prev_id, 'nid':next_id})
