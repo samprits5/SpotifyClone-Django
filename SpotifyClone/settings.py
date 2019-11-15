0265bd9a3d4b676ed183c773909cfad1463eb093
Samprit <sam@Samprits-Mac-mini.local>
2019-11-15 17:49:40 +0530

    Working on AWS S3 Configuration

--- SpotifyClone/settings.py
+++ SpotifyClone/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'SpotifyClone',
     'admin',
     'admin.login',
@@ -142,3 +143,18 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# AWS Configuration
+
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+AWS_STORAGE_BUCKET_NAME = 'django-files-bucket'
+
+AWS_S3_FILE_OVERWRITE = False
+
+AWS_DEFAULT_ACL = None
+
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+
+AWS_S3_REGION_NAME = 'us-east-2'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
